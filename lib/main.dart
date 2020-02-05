1b93ab9927c3402c4113383171d0c2b2cc72ded3
Jacob Shore <jacobshore@gmail.com>
2020-02-05 14:52:37 +0200

    finished project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDieNum = 1;
+  int rightDieNum = 1;
+
+  int rollDie() {
+    var random = new Random();
+    return 1 + random.nextInt(6);
+  }
+
+  rollDice() {
+    setState(() {
+      leftDieNum = rollDie();
+      rightDieNum = rollDie();
+    });
+    print("left: $leftDieNum, right: $rightDieNum");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: rollDice,
+              child: Image.asset('images/dice$leftDieNum.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: rollDice,
+              child: Image.asset('images/dice$rightDieNum.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
